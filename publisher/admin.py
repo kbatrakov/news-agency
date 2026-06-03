6cd1ca87532b6c55ad663b1c29d3826d33003f30
Kyrylo Batrakov <kirillbatrakov0819@gmail.com>
2026-06-03 17:38:17 +0200

    Registered models in admin.py.

--- publisher/admin.py
+++ publisher/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from publisher.models import Topic, Newspaper, Editor
 
-# Register your models here.
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display =[
+        "name",
+    ]
+
+    list_filter = [
+        "name",
+    ]
+
+    search_fields = [
+        "name",
+    ]
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "content",
+        "published_date",
+        "topic",
+        "publishers",
+    ]
+
+    list_filter = [
+        "title",
+        "published_date",
+        "topic",
+    ]
+
+    search_fields = [
+        "title",
+    ]
+
+
+@admin.register(Editor)
+class EditorAdmin(UserAdmin):
+    list_display =UserAdmin.list_display +("years_of_experience", )
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
