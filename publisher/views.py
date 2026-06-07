3474494181f9288fbf986d3d07f662bd43156471
Kyrylo Batrakov <kirillbatrakov0819@gmail.com>
2026-06-07 02:44:03 +0200

    Added 'index' function responsible for managing & displaying selected objects on the main page to the views.py file.

--- publisher/views.py
+++ publisher/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from publisher.models import Editor, Topic, Newspaper
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_editors = Editor.objects.count()
+    num_topics = Topic.objects.count()
+    num_newspapers = Newspaper.objects.count()
+
+    context = {
+        "num_editors": num_editors,
+        "num_topics": num_topics,
+        "num_newspapers": num_newspapers
+    }
+
+    return render(request, template_name="publisher/index.html", context=context)
